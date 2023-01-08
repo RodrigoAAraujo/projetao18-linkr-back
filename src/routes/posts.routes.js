235d780294eaf1df20de136265a82e327b5fe1db
CSFernandoHBK <fernandotam2@hotmail.com>
2023-01-08 09:01:35 -0300

    feat: verifyLike

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { likePost, removeLike } from "../controllers/posts.controller.js";
+import { likePost, removeLike, verifyLike } from "../controllers/posts.controller.js";
 
 const router = Router();
 
 router.post("/posts/like/:id", likePost);
 router.delete("/posts/removelike/:id", removeLike);
+router.get("/posts/likes/:id", verifyLike)
 
 export default router;
