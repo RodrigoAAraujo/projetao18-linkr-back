f9d78bb8db3a8971d64aefc3714630b4f9af0943
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-01-10 20:34:56 -0300

    testes com timeline controller

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -5,7 +5,7 @@ import joi from "joi";
 const postSchema = joi.object({
     link: joi.string().required().min(1),
     description: joi.string(),
-    //token: joi.required()
+    token: joi.required()
 });
 
 export async function postPosts(req, res){
@@ -27,8 +27,8 @@ export async function postPosts(req, res){
     //const userId = verificaToken.rows.[0].userId;
 
     try {
-        
-        await connection.query('INSERT INTO posts (link, description, "userId") VALUES ($1, $2, $3);', [post.link, post.comentary, 1]);
+        console.log(post.link, post.description)
+        await connection.query('INSERT INTO posts (link, description, "userId") VALUES ($1, $2, $3);', [post.link, post.description, 1]);
         console.log("post inserido")
         res.sendStatus(200);
         return
@@ -41,36 +41,57 @@ export async function postPosts(req, res){
 
 }
 
-export async function getPosts(req, res){
+export async function getPosts(req, res) {
 
     console.log("começou")
     let arrTimeline = [];
     let dadosLink;
 
-    //const { authorization } = req.headers;
-    //const token = authorization?.replace("Bearer ", "");
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
 
-    //const verificaToken = connection.query("SELECT * FROM sessions WHERE token=$1;", [token]);
-    //if(verificaToken.rows.lenght === 0){console.log("token inválido ou nao encontrado") res.sendStatus(400) return};
+    const verificaToken = await connection.query("SELECT * FROM sessions WHERE token=$1;", [token]);
+    console.log(verificaToken)
+    if(verificaToken.rows.length === 0){
+        console.log("token inválido ou nao encontrado") 
+        res.sendStatus(400) 
+        return
+    };
 
     try {
-        
+
         const posts = await connection.query('SELECT * FROM posts LIMIT 20;');
-        console.log(posts.rows)
         const postsReverse = posts.rows.reverse()
-        console.log(postsReverse)
-        for(let c = 0; c < postsReverse.length; c++){
+
+        for (let c = 0; c < postsReverse.length; c++) {
 
             const userId = postsReverse[c].userId;
             const link = postsReverse[c].link;
-            const comentary = postsReverse[c].comentary;
+            const comentary = postsReverse[c].description;
             const user = await connection.query('SELECT * FROM users WHERE id=$1;', [userId]);
             const imgUrl = user.rows[0].image_url;
             const name = user.rows[0].username;
 
             urlMetadata(link).then(
                 function (metadata) {
+
                     dadosLink = metadata;
+
+                    const newBody = {
+                        username: name,
+                        comentary: comentary,
+                        img: imgUrl,
+                        metadata: dadosLink
+                    }
+
+                    arrTimeline.push(newBody);
+                    console.log(posts.rows)
+                    if (c === (postsReverse.length - 1)) {
+
+                        res.send(arrTimeline);
+                        return
+                    }
+
                 },
                 function (error) {
                     console.log(error);
@@ -78,20 +99,6 @@ export async function getPosts(req, res){
                     return
                 })
 
-                const newBody = {
-                    name: name,
-                    comentary: comentary,
-                    img: imgUrl,
-                    metadata: dadosLink
-                }
-
-                arrTimeline.push(newBody);
-
-                if(c === (postsReverse.lenght - 1)){
-                    
-                    res.send(arrTimeline);
-                    return
-                }
 
         }
 
